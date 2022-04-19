ca6d3f31e9a6d947481cc902c44d6c2748f19a2b
JoaoPedroChiappa <joaopedrochiappa@edu.uniube.br>
2022-04-18 23:15:26 -0300

    Audio

--- Funcionalidades/app/src/main/java/com/example/funcionalidades/ChpAudio.java
+++ Funcionalidades/app/src/main/java/com/example/funcionalidades/ChpAudio.java
@@ -1,11 +1,24 @@
 package com.example.funcionalidades;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class ChpAudio extends AppCompatActivity {
+
+    private MediaRecorder mediaRecorder;
+    private MediaPlayer mediaPlayer;
+    private String AudioSavePath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +27,8 @@ public class ChpAudio extends AppCompatActivity {
 
         Button btnGravaAudio = (Button) findViewById(R.id.btnGravaAudio);
         Button btnReproduzAudio = (Button) findViewById(R.id.btnReproduzAudio);
+        Button btnPararGravaAudio = (Button) findViewById(R.id.btnPararGravaAudio);
+        Button btnParaReproduzAudio= (Button) findViewById(R.id.btnParaReproduzAudio);
         Button btnVoltar2 = (Button) findViewById(R.id.btnVoltar2);
 
         btnVoltar2.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +37,78 @@ public class ChpAudio extends AppCompatActivity {
                 System.exit(0);
             }
         });
+
+        btnGravaAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkPermissions() == true) {
+
+                    AudioSavePath = Environment.getExternalStorageDirectory().getAbsolutePath()
+                            +"/"+"recordingAudio.mp3";
+
+                    mediaRecorder = new MediaRecorder();
+                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                    mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+                    mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+                    mediaRecorder.setOutputFile(AudioSavePath);
+
+                    try {
+                        mediaRecorder.prepare();
+                        mediaRecorder.start();
+                        Toast.makeText(ChpAudio.this, "Gravação começou", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }else{
+                    ActivityCompat.requestPermissions(ChpAudio.this, new String[]{
+                            Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    }, 1);
+                }
+            }
+        });
+
+        btnPararGravaAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaRecorder.stop();
+                mediaRecorder.release();
+                Toast.makeText(ChpAudio.this, "Gravação finalizada", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnReproduzAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(AudioSavePath);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    Toast.makeText(ChpAudio.this, "Áudio tocando", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        btnParaReproduzAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mediaPlayer != null) {
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    Toast.makeText(ChpAudio.this, "Áudio parado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private boolean checkPermissions(){
+        int first = ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
+        int second = ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        return first == PackageManager.PERMISSION_GRANTED && second == PackageManager.PERMISSION_GRANTED;
     }
 }
